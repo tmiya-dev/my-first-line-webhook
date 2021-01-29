9104d08341d180ed41b23f6988ea3c6cd07649ad
Takeshi Miyakawa <tmiya@tmiya.dev>
2021-01-29 13:38:26 +0900

    add: index.js
    minimal version.

---
+++ index.js
@@ -0,0 +1,16 @@
+'use srtict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+  console.dir(req.body);
+  res.send('OK');
+});
+
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+console.log(`listened at port ${port}`);
+app.listen(port);
